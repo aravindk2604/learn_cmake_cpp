9eb1cc1b9435da7f0248216e433569ea9969a266
Aravind Krishnan <aravindk2604@gmail.com>
2018-08-18 01:48:12 -0700

    able to build without issues

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
+// works perfectly with just the bits/stdc++.h header
+// but otherwise the other two headers are used
+//#include <bits/stdc++.h>
 
-#include <bits/stdc++.h>
+#include <iostream> // for cout
+#include <functional> // for ref
 
-int main() 
+int main()
 {
   // intention was to use C++14 and learn CMake
-  // Hence using a lambda function here
+  // Hence using a lambda function to print Hello world 
 
   [out = std::ref(std::cout << "Hello ")](){out.get() << "World\n";}();
 
